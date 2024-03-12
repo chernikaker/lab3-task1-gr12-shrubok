4003ff77ab18f10d6992bfea279f5f23dcb04f56
Anna Shrubok <145850315+chernikaker@users.noreply.github.com>
2024-03-12 05:54:55 +0000

    main.c file updated to be a client file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-/* hello.c */
-#include <stdio.h>
+// Created by: Anna Shrubok
+// 12.03.2024
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "interface.h"
+
+int main (int argc, char *argv[])){
+
+    if(argc != 2)) {
+      printf("Error! No argument for the array size");
+      return 1;
+    }
+  
+    srand(time(NULL));
+    
+    int arraySize = atoi(argv[1]);
+    int array[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    arr[i] = rand() % 100;
+  
+    for (int i = 0; i < arraySize; i++){
+      if(isPalindrome(arr[i]))) printf("The number %d is a              palindrome\n", arr[i]");
+      else printf("The number %d is not a palindrome\n", arr[i]");
+    }
+
+    return 0;
+  
 }
