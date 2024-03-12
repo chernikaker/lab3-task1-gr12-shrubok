675e44a878e7f68871840fbef154b8e78be2019b
Anna Shrubok <145850315+chernikaker@users.noreply.github.com>
2024-03-12 06:18:08 +0000

    comments added

--- src/main.c
+++ src/main.c
@@ -6,6 +6,22 @@
 #include <stdlib.h>
 #include "interface.h"
 
+// The main function is used to test the isPalindrome function from interface.h file.
+
+//Firstly, main function checks the number of command line
+//arguments. If it does not equal to 2 (only array size), the
+//program prints an error message and returns 1. Othewise, it
+//prepares rand function and initializes an array of size equal to
+//the first command line argument. Every array element is a
+//random number from 0 to 99. Then, the main function iterates
+//over the array and calls the isPalindrome function for each
+//element and prints the result.
+
+//Input: argc - number of command line arguments, argv - array of
+//command line arguments
+
+//Output: 0 - if the program runs successfully, 1 - otherwise
+
 int main (int argc, char *argv[]){
 
     if(argc != 2) {
--- src/server.c
+++ src/server.c
@@ -3,6 +3,20 @@
 
 #include "interface.h"
 
+//Function in this file is an implementation of the interface.h
+//function and is used in main.c file
+
+//Function isPalindrome determines whether a number is a
+//palindrome. It makes a copy of the number and then reverses the
+//input number. To do this, the function uses a while loop to
+//extract the last digit of the input number and adds it to the
+//reversed number. The function then divides the input number by
+//10 to remove the last digit. The loop continues until the input
+//number is 0.
+
+//Input: number - a number
+//Output: 1 - if the number is a palindrome, 0 - otherwise
+
 int isPalindrome(int number) {
   
     int reversedNumber = 0;
